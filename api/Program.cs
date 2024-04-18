47941e3cd926f9f37b32a8ded70aa10418b7ec25
roy <roy@sittingtoad.sittingtoad>
2024-04-17 23:35:11 -0700

    Add connection string code.

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,26 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Connection string.
+var connStrBuilder = new NpgsqlConnectionStringBuilder(
+    builder.Configuration.GetConnectionString("DefaultConnection"))
+    {
+        Username = builder.Configuration["DbUsername"],
+        Password = builder.Configuration["DbPassword"]
+    };
+var connection = connStrBuilder.ConnectionString;
+
+builder.Services.AddDbContext<ApplicationDBContext>(options => {
+    options.UseNpgsql(connection);
+});
 
 var app = builder.Build();
 
