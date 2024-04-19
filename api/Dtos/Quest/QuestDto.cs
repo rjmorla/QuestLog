fa6bfd3fcfae4fa75b9327d7d9c3940f96b700ea
Roy Morla <royjmorla@gmail.com>
2024-04-19 16:00:34 -0700

    Add default Quest DTO.

---
+++ api/Dtos/Quest/QuestDto.cs
@@ -0,0 +1,11 @@
+namespace api.Dtos.Quest
+{
+    public class QuestDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        
+    }
+}
